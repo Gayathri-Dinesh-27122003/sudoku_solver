bbb01dfb9477a6eda83f3fe17f421b48168f1feb
Gayathri Dinesh <gaychidinesh@gmail.com>
2025-10-08 12:55:49 +0400

    Host Changed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from copy import deepcopy
 from solver import solve
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -57,4 +58,5 @@ def solve_api():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
